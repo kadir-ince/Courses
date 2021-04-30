92f62cb4074572b48ca99297c8f364197941bc3a
Kadir Ince <you.can.send@pm.me>
2021-04-30 13:50:32 +0300

    Initial Commit

---
+++ Shared/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Shared
+//
+//  Created by Kadir Ince on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
